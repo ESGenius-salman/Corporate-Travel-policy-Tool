dab455c0df1bb0623798cd06866d2bc2ff641529
V-shalini22 <shalinivarada12@gmail.com>
2025-10-27 11:37:38 +0530

    login by shalu

--- Travel_backend/modules/safety/alert.model.js
+++ Travel_backend/modules/safety/alert.model.js
@@ -1,5 +1,5 @@
 // If alert.model.js is in modules/safety/
-const sequelize = require("../../config/db");
+/*const sequelize = require("../../config/db");
 const { DataTypes } = require("sequelize");
 
 // define Alert model
@@ -10,6 +10,24 @@ const Alert = sequelize.define("Alert", {
   notified: { type: DataTypes.BOOLEAN, defaultValue: false },
   seen: { type: DataTypes.BOOLEAN, defaultValue: false },
   userId: { type: DataTypes.INTEGER, allowNull: false },
+});
+
+module.exports = Alert;
+*/
+// If alert.model.js is in modules/safety/
+const sequelize = require("../../config/db");
+const { DataTypes } = require("sequelize");
+
+// define Alert model
+const Alert = sequelize.define("Alert", {
+  // columns
+  alertType: { type: DataTypes.STRING, allowNull: false },
+  message: { type: DataTypes.STRING, allowNull: false },
+  notified: { type: DataTypes.BOOLEAN, defaultValue: false },
+  seen: { type: DataTypes.BOOLEAN, defaultValue: false },
+
+  // temporarily allow NULL until existing data fixed
+  userId: { type: DataTypes.INTEGER, allowNull: true },
 });
 
 module.exports = Alert;
