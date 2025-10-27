4b4a3dd55a7114ea9b3e01c2229f766a6a15b60f
V-shalini22 <shalinivarada12@gmail.com>
2025-10-27 10:42:29 +0530

    shalini

--- Travel_frontend/src/context/AuthContext.js
+++ Travel_frontend/src/context/AuthContext.js
@@ -45,7 +45,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("http://localhost:5000/auth/login", {
         email,
         password,
       });
